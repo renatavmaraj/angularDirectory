9ada8c427dd007a22b0293136c095195fe3b186f
Renata Maraj <renatavmaraj@gmail.com>
2017-01-25 20:02:35 -0800

    add partials

--- js/controllers.js
+++ js/controllers.js
@@ -7,6 +7,7 @@ myApp.controller('MyController', ['$scope', '$http', function ($scope, $http){
   $http.get('js/data.json').success(function(data) {
     //Assign the data received from json file to $scope.artists
     $scope.artists = data;
+    $scope.artistOrder = 'name';
   });
 
 }]);
